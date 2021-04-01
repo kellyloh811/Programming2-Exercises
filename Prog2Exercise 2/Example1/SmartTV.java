a6293e23624c68d175522aed8d82caf4354b1ca7
lohsiewchin <73092542+lohsiewchin@users.noreply.github.com>
2021-04-01 14:35:27 +0800

    Add files via upload

---
+++ Prog2Exercise 2/Example1/SmartTV.java
@@ -0,0 +1,20 @@
+package Example1;
+
+public class SmartTV {
+
+	String Brand = "Panasonic";
+	String Model = "TH-65HZ1500K";
+	String Screen = "OLED";
+	
+	void Youtube () {
+		System.out.println("On Youtube.");
+	}
+	
+	void Browser () {
+		System.out.println("On Browser.");
+	}
+	
+	void ShareScreen () {
+		System.out.println("Start to share screen.");
+	}
+}
